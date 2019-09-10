591ac235a559b6cd155835a09e746418cf865521
Gert Drapers <gert.drapers@live.com>
2019-09-09 20:06:27 -0700

    add data gathering script for baseline tests

---
+++ getdata.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd ./data
+
+curl -O https://ikeptwalking.com/wp-content/uploads/2017/04/Employees50K.zip
+curl -O https://ikeptwalking.com/wp-content/uploads/2017/04/Employees100K.zip
+curl -O https://download.elastic.co/demos/kibana/gettingstarted/7.x/shakespeare.json
+curl -O https://download.elastic.co/demos/kibana/gettingstarted/7.x/accounts.zip
+curl -O https://download.elastic.co/demos/kibana/gettingstarted/7.x/logs.jsonl.gz
+
+cd ..
